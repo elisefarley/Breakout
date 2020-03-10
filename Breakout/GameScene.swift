5efe944655cca26e4a008d4a81c519615521523d
Elise Farley <efarley@bsd220.org>
2020-03-10 10:43:22 -0500

    Created brick object

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -13,11 +13,13 @@ class GameScene: SKScene {
     
     var ball = SKShapeNode()
     var paddle = SKSpriteNode()
+    var brick =  SKSpriteNode()
     
     override func didMove(to view: SKView) {
         createBackground()
         makeBall()
         makePaddle()
+        makeBrick()
     }
     
     func createBackground() {
@@ -70,4 +72,12 @@ class GameScene: SKScene {
        addChild(paddle)
     }
 
+    func makeBrick() {
+       brick = SKSpriteNode(color: .blue, size: CGSize(width: 50, height: 20))
+       brick.position = CGPoint(x: frame.midX, y: frame.maxY - 50)
+       brick.name = "brick"
+       brick.physicsBody = SKPhysicsBody(rectangleOf: brick.size)
+       brick.physicsBody?.isDynamic = false
+       addChild(brick)
+    }
 }
